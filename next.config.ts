1542f4dbe245602b357b63b6b871d6230d7c76d4
Vinan <vinanjayasinghe3@gmail.com>
2026-03-29 23:26:11 +0530

    revert to direct API URL

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,5 @@
 import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "http://34.124.138.25/:path*",
-      },
-    ];
-  },
 };
 export default nextConfig;
