39ea6e6e5ef6f2dfd64daf878adb01dae724112d
Vinan <vinanjayasinghe3@gmail.com>
2026-03-29 23:18:53 +0530

    use Next.js rewrites for API proxy

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,13 @@
 import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://34.124.138.25/:path*",
+      },
+    ];
+  },
 };
 export default nextConfig;
